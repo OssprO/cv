bb30198d885cb48caf641aabb294ad430c063f7b
Hernandez Gonzalez <jlhl@gft.com>
2019-08-06 10:40:20 -0500

    update(data): updating new data

--- src/app/components/time-circle/time-circle.component.ts
+++ src/app/components/time-circle/time-circle.component.ts
@@ -12,19 +12,19 @@ export class TimeCircleComponent implements OnInit {
   @ViewChild('TimeCircle') timeCircle: ElementRef;
   private timeCircleContext: CanvasRenderingContext2D;
 
-  private GRADOS_APERTURA:number = 295; //DEGREES
-  private CANVAS_SIZE:number = 800;
-  private RADIO:number = this.CANVAS_SIZE/2.075;
-  private ADJUSTMENT:number = -40;
+  private GRADOS_APERTURA = 295; //DEGREES
+  private CANVAS_SIZE = 800;
+  private RADIO = this.CANVAS_SIZE / 2.075;
+  private ADJUSTMENT = -40;
 
-  protected initialYear:number = 2005;
-  protected finalYear:number = 2018;
-  protected difYears:number;
-  protected gradosYear:number;
+  protected initialYear = 2005;
+  protected finalYear = 2019;
+  protected difYears: number;
+  protected gradosYear: number;
 
-  private experiencia:any;
+  private experiencia: any;
 
-  constructor( private _experienciaService:ExperienciaService) { }
+  constructor( private _experienciaService: ExperienciaService) { }
 
   ngOnInit() {
     this.timeCircleContext = (this.timeCircle.nativeElement as HTMLCanvasElement).getContext('2d');
@@ -38,54 +38,54 @@ export class TimeCircleComponent implements OnInit {
   }
 
   private drawYearLines() {
-    let difYears = this.finalYear - this.initialYear + 1;
-    let gradosYear = this.GRADOS_APERTURA / difYears;
+    const difYears = this.finalYear - this.initialYear + 1;
+    const gradosYear = this.GRADOS_APERTURA / difYears;
     let currentYear = this.initialYear;
 
-    let lineLong = this.RADIO/3;
+    const lineLong = this.RADIO / 3;
 
-    for( var i=0; i<=difYears; i++ ){
-      var angle = Mathematical.radians((gradosYear*i) + this.ADJUSTMENT);
+    for ( let i = 0; i <= difYears; i++ ) {
+      const angle = Mathematical.radians((gradosYear * i) + this.ADJUSTMENT);
       this.timeCircleContext.lineWidth = 1;
       this.timeCircleContext.beginPath();
 
-      var x1 = (this.CANVAS_SIZE/2) + Math.cos(angle) * (this.RADIO);
-      var y1 = (this.CANVAS_SIZE/2) + Math.sin(angle) * (this.RADIO);
-      var x2 = (this.CANVAS_SIZE/2) + Math.cos(angle) * (this.RADIO - (lineLong));
-      var y2 = (this.CANVAS_SIZE/2) + Math.sin(angle) * (this.RADIO - (lineLong));
+      const x1 = (this.CANVAS_SIZE / 2) + Math.cos(angle) * (this.RADIO);
+      const y1 = (this.CANVAS_SIZE / 2) + Math.sin(angle) * (this.RADIO);
+      const x2 = (this.CANVAS_SIZE / 2) + Math.cos(angle) * (this.RADIO - (lineLong));
+      const y2 = (this.CANVAS_SIZE / 2) + Math.sin(angle) * (this.RADIO - (lineLong));
 
       this.timeCircleContext.moveTo(x1, y1);
       this.timeCircleContext.lineTo(x2, y2);
       this.timeCircleContext.strokeStyle = '#D0D0D0';
       this.timeCircleContext.stroke();
 
-      this.timeCircleContext.font = "38px";
-      this.timeCircleContext.fillStyle = "#333333";
-      this.timeCircleContext.fillText(currentYear.toString(), x1-5, y1);
+      this.timeCircleContext.font = '38px';
+      this.timeCircleContext.fillStyle = '#333333';
+      this.timeCircleContext.fillText(currentYear.toString(), x1 - 5, y1);
 
       currentYear++;
     }
   }
 
   private parseData(data) {
-    for (let educacion of this._experienciaService.getEducacion()) {
-      this.drawArc( this.RADIO*0.80, this.RADIO*0.065, educacion.color, educacion.inicio, educacion.final );
+    for (const educacion of this._experienciaService.getEducacion()) {
+      this.drawArc( this.RADIO * 0.80, this.RADIO * 0.065, educacion.color, educacion.inicio, educacion.final );
     }
-    for (let freelance of this._experienciaService.getFreelances()) {
-      this.drawArc( this.RADIO*0.725, this.RADIO*0.05, freelance.color, freelance.inicio, freelance.final );
+    for (const freelance of this._experienciaService.getFreelances()) {
+      this.drawArc( this.RADIO * 0.725, this.RADIO * 0.05, freelance.color, freelance.inicio, freelance.final );
     }
-    for (let trabajo of this._experienciaService.getExperienciaLaboral()) {
-      this.drawArc( this.RADIO*0.90, this.RADIO*0.10, trabajo.color, trabajo.inicio, trabajo.final );
+    for (const trabajo of this._experienciaService.getExperienciaLaboral()) {
+      this.drawArc( this.RADIO * 0.90, this.RADIO * 0.10, trabajo.color, trabajo.inicio, trabajo.final );
     }
   }
 
   private drawArc( radio, grueso, color, inicio, final) {
-    let difYears = this.finalYear - this.initialYear + 1;
-    let gradosYear = this.GRADOS_APERTURA / difYears;
-    let gradosMonth = gradosYear / 12;
+    const difYears = this.finalYear - this.initialYear + 1;
+    const gradosYear = this.GRADOS_APERTURA / difYears;
+    const gradosMonth = gradosYear / 12;
 
-    let inicioDate = new Date(inicio);
-    let finalDate = new Date(final);
+    const inicioDate = new Date(inicio);
+    const finalDate = new Date(final);
 
     let initialAngle =  Mathematical.radians(gradosYear*(inicioDate.getFullYear()-this.initialYear) + gradosMonth*(inicioDate.getMonth()) + this.ADJUSTMENT);
     let finalAngle =  Mathematical.radians(gradosYear*(finalDate.getFullYear()-this.initialYear) + gradosMonth*(finalDate.getMonth()) + this.ADJUSTMENT);
@@ -93,8 +93,8 @@ export class TimeCircleComponent implements OnInit {
     this.timeCircleContext.lineWidth = grueso;
     this.timeCircleContext.beginPath();
     this.timeCircleContext.arc(
-      this.CANVAS_SIZE/2,
-      this.CANVAS_SIZE/2,
+      this.CANVAS_SIZE / 2,
+      this.CANVAS_SIZE / 2,
       radio,
       initialAngle,
       finalAngle,
@@ -106,10 +106,10 @@ export class TimeCircleComponent implements OnInit {
   // Converts from degrees to radians.
   public mathRadians(degrees) {
     return degrees * Math.PI / 180;
-  };
+  }
 
   // Converts from radians to degrees.
   public mathDegrees(radians) {
     return radians * 180 / Math.PI;
-  };
+  }
 }
--- src/app/services/experiencia.service.ts
+++ src/app/services/experiencia.service.ts
@@ -6,152 +6,159 @@ import { Trabajo } from '../interfaces/trabajo.interface';
 @Injectable()
 export class ExperienciaService {
 
-  private experienciaObj = {
+
+/**
+ *
+ * Nota: Las fechas se expresan en formato gringo: MM/dd/YYYY
+ * @private
+ * @memberof ExperienciaService
+ */
+private experienciaObj = {
       educacion: [
         {
-            escuela: "Instituto Tecnológico de Durango",
-            lugar: "Durango, Dgo. México",
-            titulo: "Ingeniería en Sistemas Computacionales",
-            cedula: "8500626",
-            inicio: "01-01-2006",
-            final: "06-01-2010",
-            color: "#000000"
+            escuela: 'Instituto Tecnológico de Durango',
+            lugar: 'Durango, Dgo. México',
+            titulo: 'Ingeniería en Sistemas Computacionales',
+            cedula: '8500626',
+            inicio: '01-01-2006',
+            final: '06-01-2010',
+            color: '#000000'
         },
         {
-            escuela: "CBTIS 130",
-            lugar: "Durango, Dgo. México",
-            titulo: "Técnico en Computación",
-            cedula: "5183946",
-            inicio: "01-01-2005",
-            final: "12-01-2005",
-            color: "#000000"
+            escuela: 'CBTIS 130',
+            lugar: 'Durango, Dgo. México',
+            titulo: 'Técnico en Computación',
+            cedula: '5183946',
+            inicio: '01-01-2005',
+            final: '12-01-2005',
+            color: '#000000'
         }
       ],
       freelance: [
         {
-            nombre: "Brewer Map",
-            lugar: "CDMX, México",
-            descripcion: "Proyecto personal. Desarrollo multiplataroma. Diseño, creación y mantenimiento de blog WordPress, app MEAN stack, redes sociales y contenidos.",
-            inicio: "07-01-2017",
-            final: "08-01-2018",
-            color: "#776E5C",
-            tecnologias: ["Angular", "NodeJS/Restify/Express", "MongoDB/Mongoose"]
+            nombre: 'Brewer Map',
+            lugar: 'CDMX, México',
+            descripcion: 'Proyecto personal. Desarrollo multiplataroma. Diseño, creación y mantenimiento de blog WordPress, app MEAN stack, redes sociales y contenidos.',
+            inicio: '07-01-2017',
+            final: '08-01-2018',
+            color: '#776E5C',
+            tecnologias: ['Angular', 'NodeJS/Restify/Express', 'MongoDB/Mongoose']
         },
         {
-            nombre: "GICSA",
-            lugar: "CDMX, México",
-            descripcion: "Maquetación y adaptación resposiva de GICSA MagazINe.",
-            inicio: "04-01-2017",
-            final: "06-01-2017",
-            color: "#C3A15C",
-            tecnologias: ["SASS"] 
+            nombre: 'GICSA',
+            lugar: 'CDMX, México',
+            descripcion: 'Maquetación y adaptación resposiva de GICSA MagazINe.',
+            inicio: '04-01-2017',
+            final: '06-01-2017',
+            color: '#C3A15C',
+            tecnologias: ['SCSS']
         },
         {
-            nombre: "Mercedes Trophy",
-            lugar: "CDMX, México",
-            descripcion: "Maquetación, adaptación resposiva y posterior adaptación a theme de WordPress para sitio Mercedes Trophy México 2016.",
-            inicio: "04-01-2016",
-            final: "06-01-2016",
-            color: "#E0C4A1",
-            tecnologias: ["WordPress","SASS"] 
+            nombre: 'Mercedes Trophy',
+            lugar: 'CDMX, México',
+            descripcion: 'Maquetación, adaptación resposiva y posterior adaptación a theme de WordPress para sitio Mercedes Trophy México 2016.',
+            inicio: '04-01-2016',
+            final: '06-01-2016',
+            color: '#E0C4A1',
+            tecnologias: ['WordPress', 'SCSS']
         },
         {
-            nombre: "CONAGUA",
-            lugar: "CDMX, México",
-            descripcion: "Diseño y programación de sistema de búsqueda de documentos de la Comisión Nacional del Agua (Flex/AIR/SQLLite).",
-            inicio: "03-01-2012",
-            final: "01-01-2013",
-            color: "#F7E4BE",
-            tecnologias: ["AS3/Flex/AIR"]
+            nombre: 'CONAGUA',
+            lugar: 'CDMX, México',
+            descripcion: 'Diseño y programación de sistema de búsqueda de documentos de la Comisión Nacional del Agua (Flex/AIR/SQLLite).',
+            inicio: '03-01-2012',
+            final: '01-01-2013',
+            color: '#F7E4BE',
+            tecnologias: ['AS3/Flex/AIR']
         },
         {
-            nombre: "YEAL/Artefacto",
-            lugar: "Durango, Dgo. México",
-            descripcion: "Prácticas profesionales, desarrollo y diseño del juego ‘PokTaPok’ para iPhone y iPod Touch.  Subcontrato de la empresa de creación multimedia Artefacto Studio.",
-            inicio: "01-01-2010",
-            final: "07-01-2010",
-            color: "#B7A98D",
-            tecnologias: ["Photoshop", "Unity3D"]
+            nombre: 'YEAL/Artefacto',
+            lugar: 'Durango, Dgo. México',
+            descripcion: 'Prácticas profesionales, desarrollo y diseño del juego ‘PokTaPok’ para iPhone y iPod Touch.  Subcontrato de la empresa de creación multimedia Artefacto Studio.',
+            inicio: '01-01-2010',
+            final: '07-01-2010',
+            color: '#B7A98D',
+            tecnologias: ['Photoshop', 'Unity3D']
         },
         {
-            nombre: "OptimusTEC",
-            lugar: "Durango, Dgo. México",
-            descripcion: "Fundador, diseñador y encargado de maquetación de la revista estudiantíl OptimusTEC, diseño de logotipo, diseño de lonas, fotógrafo y edición fotográfica, redacción de artículos, gestor de promoción y diseñador del sitio oficial de la revista.",
-            inicio: "08-01-2006",
-            final: "12-01-2009",
-            color: "#776E5C",
-            tecnologias: ["InDesing","Photoshop"]
+            nombre: 'OptimusTEC',
+            lugar: 'Durango, Dgo. México',
+            descripcion: 'Fundador, diseñador y encargado de maquetación de la revista estudiantíl OptimusTEC, diseño de logotipo, diseño de lonas, fotógrafo y edición fotográfica, redacción de artículos, gestor de promoción y diseñador del sitio oficial de la revista.',
+            inicio: '08-01-2006',
+            final: '12-01-2009',
+            color: '#776E5C',
+            tecnologias: ['InDesing', 'Photoshop']
         }
       ],
       trabajo: [
         {
-            nombre: "GFT México",
-            puesto: "Desarrollador Front-End",
-            lugar: "CDMX, México",
-            descripcion: "Desarrollo front-end de plataforma iBrain. Proyecto Cells BBVBA Bancomer.",
-            inicio: "03-05-2018",
-            final: "08-09-2018",
-            color: "#A1755B", 
-            tecnologias: ["Angular", "SCSS"]
+            nombre: 'GFT México',
+            puesto: 'Líder Front-End / Desarrollador Front-End ',
+            lugar: 'CDMX, México',
+            descripcion: 'Líder técnico Front-End para proyecto Santander Móvil, al frente de 6 células de trabajo y responsable de integración y despliegue de una app híbrida para iOS y Android. Desarrollo front-end de plataforma iBrain. Proyecto Cells BBVBA Bancomer.',
+            inicio: '03-05-2018',
+            final: '08-01-2019',
+            color: '#A1755B',
+            tecnologias: ['Angular', 'SCSS']
         },
         {
-            nombre: "Apernet S.A. / Minimalist Technology",
-            puesto: "Desarrollador Front-End",
-            lugar: "CDMX, México",
-            descripcion: "Desarrollo Smart TV, administrador y creador de contenido de la comunidad de desarrolladores Samsung Smart TV. Desarrollo y diseño web, creación de temas y plugins para WordPress. Desarrollo Mobile Ionic y Phonegaph. Desarrollo MEAN Stack.",
-            inicio: "09-01-2013",
-            final: "02-28-2018",
-            color: "#EEBC9E",
-            tecnologias: ["AS3/Flex/AIR", "Ionic", "WordPress" ]
+            nombre: 'Apernet S.A. / Minimalist Technology',
+            puesto: 'Desarrollador Front-End',
+            lugar: 'CDMX, México',
+            descripcion: 'Desarrollo Smart TV, administrador y creador de contenido de la comunidad de desarrolladores Samsung Smart TV. Desarrollo y diseño web, creación de temas y plugins para WordPress. Desarrollo Mobile Ionic y Phonegaph. Desarrollo MEAN Stack.',
+            inicio: '09-01-2013',
+            final: '02-28-2018',
+            color: '#EEBC9E',
+            tecnologias: ['AS3/Flex/AIR', 'Ionic', 'WordPress' ]
         },
         {
-            nombre: "Denumeris Interactive",
-            puesto: "Desarrollador Interactivo",
-            lugar: "CDMX, México",
-            descripcion: "Desarrollo multimedia, programación de interfaces multitouch para muros y pantallas interactivas , así como aplicaciones móviles. Creación de sitios web y aplicaciones de escritorio Flash/ActionScript/Flex. Programación del modulo interactivo Periscopio en el Museo de Arte Contemporaneo de la UNAM.",
-            inicio: "10-01-2010",
-            final: "08-01-2013",
-            color: "#B38184",
-            tecnologias: ["AS3/Flex/AIR", "Swift"]
+            nombre: 'Denumeris Interactive',
+            puesto: 'Desarrollador Interactivo',
+            lugar: 'CDMX, México',
+            descripcion: 'Desarrollo multimedia, programación de interfaces multitouch para muros y pantallas interactivas , así como aplicaciones móviles. Creación de sitios web y aplicaciones de escritorio Flash/ActionScript/Flex. Programación del modulo interactivo Periscopio en el Museo de Arte Contemporaneo de la UNAM.',
+            inicio: '10-01-2010',
+            final: '08-01-2013',
+            color: '#B38184',
+            tecnologias: ['AS3/Flex/AIR', 'Swift']
         },
         {
-            nombre: "Departamento de Desarrollo de Sistemas y Administración de Información Secretaría de Educación, Estado de Durango.",
-            puesto: "Desarrollador",
-            lugar: "Durango, Dgo. México",
-            descripcion: "Desarrollo y diseño de aplicaciones y sistemas administrativos para control y mantenimiento de bases de datos.",
-            inicio: "01-01-2010",
-            final: "09-01-2010",
-            color: "#786573",
-            tecnologias: ["PHP"]
+            nombre: 'Departamento de Desarrollo de Sistemas y Administración de Información Secretaría de Educación, Estado de Durango.',
+            puesto: 'Desarrollador',
+            lugar: 'Durango, Dgo. México',
+            descripcion: 'Desarrollo y diseño de aplicaciones y sistemas administrativos para control y mantenimiento de bases de datos.',
+            inicio: '01-01-2010',
+            final: '09-01-2010',
+            color: '#786573',
+            tecnologias: ['PHP']
         },
         {
-            nombre: "Oficina de Asesoría en Modernización y Tecnologías de Información Gobierno Municipal de Durango",
-            puesto: "Desarrollador/Diseñador Web",
-            lugar: "Durango, Dgo. México",
-            descripcion: "Realización de presentaciones multimedia para diversas dependencias de gobierno, video streaming, contenido interactivo Flash en el sitio, diseño gráfico y maquetación, fotografía y edición fotográfica, diseño de elementos para diversos sistemas de información.",
-            inicio: "11-01-2007",
-            final: "11-01-2008",
-            color: "#49414E",
-            tecnologias: ["AS3/Flex/AIR", "Illustrator", "Photoshop"]
+            nombre: 'Oficina de Asesoría en Modernización y Tecnologías de Información Gobierno Municipal de Durango',
+            puesto: 'Desarrollador/Diseñador Web',
+            lugar: 'Durango, Dgo. México',
+            descripcion: 'Realización de presentaciones multimedia para diversas dependencias de gobierno, video streaming, contenido interactivo Flash en el sitio, diseño gráfico y maquetación, fotografía y edición fotográfica, diseño de elementos para diversos sistemas de información.',
+            inicio: '11-01-2007',
+            final: '11-01-2008',
+            color: '#49414E',
+            tecnologias: ['AS3/Flex/AIR', 'Illustrator', 'Photoshop']
         },
       ],
     };
 
   constructor() { }
 
-  getExperiencia():object {
+  getExperiencia(): object {
     return this.experienciaObj;
   }
 
-  getEducacion():Educacion[] {
+  getEducacion(): Educacion[] {
     return this.experienciaObj.educacion;
   }
 
-  getFreelances():Freelance[] {
+  getFreelances(): Freelance[] {
     return this.experienciaObj.freelance;
   }
 
-  getExperienciaLaboral():Trabajo[] {
+  getExperienciaLaboral(): Trabajo[] {
     return this.experienciaObj.trabajo;
   }
 
